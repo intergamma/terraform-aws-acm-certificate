8df1e21b9dd8eb1eb1843485c50342d8cb4cf296
Rutger van Bruggen <rvanbruggen@mirabeau.nl>
2019-02-18 11:02:52 +0100

    update

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "domain_name" {
+  type        = "string"
+  description = "A domain name for which the certificate should be issued"
+}
+
+variable "validation_method" {
+  type        = "string"
+  default     = "DNS"
+  description = "Which method to use for validation, DNS or EMAIL"
+}
+
+variable "process_domain_validation_options" {
+  type        = "string"
+  default     = "true"
+  description = "Flag to enable/disable processing of the record to add to the DNS zone to complete certificate validation"
+}
+
+variable "ttl" {
+  type        = "string"
+  default     = "300"
+  description = "The TTL of the record to add to the DNS zone to complete certificate validation"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+}
+
+variable "subject_alternative_names" {
+  type        = "list"
+  default     = []
+  description = "A list of domains that should be SANs in the issued certificate"
+}
+
+variable "zone_name" {
+  type        = "string"
+  default     = ""
+  description = "The name of the desired Route53 Hosted Zone"
+}
