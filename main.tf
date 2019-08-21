26981ea7a9aab58defbdc358c94fb222ce10b8a6
Rutger van Bruggen <rvanbruggen@mirabeau.nl>
2019-08-21 15:11:43 +0200

    update code to terraform 0.12

--- main.tf
+++ main.tf
@@ -11,24 +11,24 @@ resource "aws_acm_certificate" "default" {
 }
 
 data "aws_route53_zone" "default" {
-  name         = "${var.zone_name}"
+  name         = var.zone_name
   private_zone = false
 }
 
 resource "aws_acm_certificate_validation" "default" {
   provider        = "aws.eu-west-1"
-  certificate_arn = "${aws_acm_certificate.default.arn}"
+  certificate_arn = aws_acm_certificate.default.arn
 
   validation_record_fqdns = aws_route53_record.default[*].fqdn
 }
 
 resource "aws_route53_record" "default" {
-  count   = "${length(var.subject_alternative_names) + 1}"
-  zone_id = "${data.aws_route53_zone.default.zone_id}"
-  name    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")}"
-  type    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
-  ttl     = "${var.ttl}"
-  records = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index],"resource_record_value")}"]
-  
+  count   = length(var.subject_alternative_names) + 1
+  zone_id = data.aws_route53_zone.default.zone_id
+  name    = lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")
+  type    = lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")
+  ttl     = var.ttl
+  records = [lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")]
+
   allow_overwrite = true
 }
