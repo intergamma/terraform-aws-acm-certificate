e530ada5022ca2d629666bc94a68a6a7a24bd737
Rutger van Bruggen <rvanbruggen@mirabeau.nl>
2019-08-21 14:18:09 +0200

    update code to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_acm_certificate" "default" {
   provider                  = "aws.eu-west-1"
-  domain_name               = "${var.domain_name}"
-  validation_method         = "${var.validation_method}"
-  subject_alternative_names = ["${var.subject_alternative_names}"]
-  tags                      = "${var.tags}"
+  domain_name               = var.domain_name
+  validation_method         = var.validation_method
+  subject_alternative_names = var.subject_alternative_names
+  tags                      = var.tags
 
   lifecycle {
     create_before_destroy = true
